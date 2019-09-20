ec18d8830f6d0f4af37c28153bdffa00bf9b7666
ArtemGomer <43440717+ArtemGomer@users.noreply.github.com>
2019-09-20 20:43:04 +0700

    Update Lab1.cpp

--- EvmPu/Lab1.cpp
+++ EvmPu/Lab1.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <cmath>
 #include <random>
-#include <chrono>
+#include <time.h>
 using namespace std;
 
-class pi{
+class pi {
  private:
-  static float getNumber(){
-    return (rand() / (float)RAND_MAX) * (rand() % 2 == 0 ? 1 : (-1));
+  static float getNumber() {
+    return (rand() / (float) RAND_MAX) * (rand() % 2 == 0 ? 1 : (-1));
   }
  public:
-  static float printPi(unsigned long long N){
+  static float printPi(int N) {
     unsigned long long inCircle = 0;
-    for (int i = 0; i < N; i++){
-      if ((powf(getNumber(), 2) + powf(getNumber(), 2)) <= 1){
+    for (int i = 0; i < N; i++) {
+      if ((powf(getNumber(), 2) + powf(getNumber(), 2)) <= 1) {
         inCircle++;
       }
     }
-    return (4 * inCircle)/ (float)N;
+    return (4 * inCircle) / (float) N;
   }
 };
 
-int main(int argc, char **argv){
-  auto start = std::chrono::system_clock::now();
+int main(int argc, char **argv) {
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
   cout << pi::printPi(atoi(argv[1])) << endl;
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end - start;
-  cout << elapsed_seconds.count() << 's' << endl;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  cout << "Time taken: " << to_string(end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec)) << endl;
   return 0;
 }
